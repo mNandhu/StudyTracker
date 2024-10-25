b33ebfd83edc5e5b20542bc8a9035abf4b1a1f4e
Kiruthik Nandhan Murthi <38995539+mNandhu@users.noreply.github.com>
2024-10-25 17:30:48 +0530

    Removed components that cause bugs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, {isServer}) => {
+        if (!isServer) {
+            config.resolve.fallback = {
+                child_process: false,
+                fs: false,
+                net: false,
+                tls:false,
+                dns:false,
+                '@napi-rs/snappy': false,
+                'timers/promises': false,
+            };
+        }
+        return config;
+    },
+};
 
 export default nextConfig;
