84143d5bd51d6988f92909df877c93a97fdc1fb9
Kiruthik Nandhan Murthi <38995539+mNandhu@users.noreply.github.com>
2024-10-25 17:30:25 +0530

    Updated to new Sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,55 +1,32 @@
 'use client';
 import React from 'react'
 import {ThemeProvider} from '@/components/theme-provider'
-import Sidebar from '@/components/sidebar'
-import {Button} from '@/components/ui/button'
-import {Moon, Sun} from 'lucide-react'
-import {useTheme} from 'next-themes'
+
 import '@/app/globals.css'
+import AppSidebar from "@/components/sidebar";
+import {SidebarProvider} from "@/components/ui/sidebar"
 
 export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
         <html lang="en" suppressHydrationWarning>
-        <body className="bg-background text-foreground">
+        <body className="bg-background text-foreground scroll-container">
         <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
         >
-            <div className="flex h-screen">
-                <div className={"w-36 mr-4"}>
-                    <Sidebar/>
-                </div>
-                <main className="flex-1 overflow-y-auto p-8">
-                    <div className="container mx-auto">
-                        <div className="flex justify-between items-center mb-8">
-                            <h1 className="text-4xl font-bold ">StudyTracker</h1>
-                            <ThemeToggle/>
-                        </div>
+            <SidebarProvider defaultOpen={true}>
+                <AppSidebar/>
+                <main className="ml-4 flex-1 flex flex-col items-center">
+                    <div className="container mx-auto w-full">
                         {children}
                     </div>
                 </main>
-            </div>
+            </SidebarProvider>
+
         </ThemeProvider>
         </body>
         </html>
-    )
-}
-
-function ThemeToggle() {
-    const {setTheme, theme} = useTheme()
-
-    return (
-        <Button
-            variant="default"
-            size="icon"
-            onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-        >
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"/>
-            <Moon
-                className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
-            <span className="sr-only">Toggle theme</span>
-        </Button>
     )
 }
